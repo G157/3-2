83d7048bdcbd2953a1adad4a9f2f670ad8bc22cd
g15907hy <g15907hy@s30415.tsuda.ac.jp>
2017-04-28 11:52:45 +0900

    First

---
+++ test/snake/SnakeCamelUtilTest.java
@@ -0,0 +1,14 @@
+package snake;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class SnakeCamelUtilTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
